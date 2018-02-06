e703265788cc2f3aa4bc713cd48929064ba601ee
SheepRanger <milk500ted939@gmail.com>
2018-02-06 18:39:37 +0800

    Graph interface

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -11,18 +11,19 @@
 #include "rc_tree.h"
 
 typedef enum { RC_TREE, IN_CELL } Edge_type;
-const string input_prefix  = "primary_in";
-const string output_prefix = "primary_out";
+const string INPUT_PREFIX  = "primary_in";
+const string OUTPUT_PREFIX = "primary_out";
 
 class Graph {
 
 public:
 	
 	struct Node {
-		// Temporarily nothing to store!?
-		// Maybe store dp information
+		bool exist;
 		int index;
 		string name;
+		float at[2][2];  // Arrival time. e.g, at[EARLY][RISE]
+		float rat[2][2]; // Required arrival time.
 		
 		Node(int index, const string& name);
 	};
@@ -45,30 +46,36 @@ public:
 	};
 	
 	// Node related
-	int get_index(const string &name);                        // "name" should follow this format: <Cell name>:<Pin name>, insert one if not found.
-	const string& get_name(int index) const;                  // Get name from index.
+	int get_index(const string &name);                 // "name" should follow this format: <Cell name>:<Pin name>, insert one if not found.
+	const string& get_name(int index) const;           // Get name from index.
+	bool in_graph(int index) const;                    // Check whether a node with "index" in graph
+	bool in_graph(const string &name) const;           // Check whether a node with "name" in graph (name: <cell_name>:<pin_name>)
+	void set_at(const string &pin_name, float early_at[2], float late_at[2]); // Set arrival time of given pin, only primary input is allowed to be set by this function
+	void set_rat(const string &pin_name, float early_rat[2], float late_rat[2]);    // Only primary output can be set by this function
 	int add_node(const string &name);
 	
 	// Edge related
-	const vector< unordered_map<int, Edge*> >& adj_list() const;// Return adjacency list of node i.
-	Edge* add_edge(int src, int dest, Edge_type type);          // Add an empty edge, with neither arc nor rc tree.
-	Edge* get_edge(int src, int dest);                          // Return NULL if not exist
-	void add_arc(int src, int dest, TimingArc *arc, Mode mode); // Append one arc to edge from src to dest, add edge if doesn't exist.
+	const vector< unordered_map<int, Edge*> >& adj_list() const;     // Return adjacency list
+	const vector< unordered_map<int, Edge*> >& rev_adj_list() const; // Return reverse adjacency list
+	Edge* add_edge(int src, int dest, Edge_type type);               // Add an empty edge, with neither arc nor rc tree.
+	Edge* get_edge(int src, int dest) const;                         // Return NULL if not exist
+	void add_arc(int src, int dest, TimingArc *arc, Mode mode);      // Append one arc to edge from src to dest, add edge if doesn't exist.
 	
 	// Wire related
-	Wire_mapping* get_wire_mapping(const string &wire_name);
+	Wire_mapping* get_wire_mapping(const string &wire_name) const;
 	
 	// Graph related
 	void build(Verilog &vlog, Spef &spef, CellLib &early_lib, CellLib &late_lib); // Build this graph from a verilog file.
 	
 private:
 	int next_id;
-	unordered_map<string,int> trans;         // Transform name to index.
-	vector<Node> nodes;                      // nodes[i]: Node with index i.
-	vector< unordered_map<int, Edge*> > adj; // Adjacency list of all nodes.
+	unordered_map<string,int> trans;             // Transform name to index.
+	vector<Node> nodes;                          // nodes[i]: Node with index i.
+	vector< unordered_map<int, Edge*> > adj;     // Adjacency list of all nodes.
+	vector< unordered_map<int, Edge*> > rev_adj; // Reverse adjacency list.
 	
-	// Used when building graph, not sure if this will be used in the future.
-	// Key  = wire name
+	// "wire_mapping":
+	// key  = wire name
 	// src  = output pin index (source pin)
 	// sink = vector of input pin index (sink pins)
 	unordered_map<string, Wire_mapping*> wire_mapping;
