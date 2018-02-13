f99aef0f384459fbd961d8c72f7d0a847b266d22
SheepRanger <milk500ted939@gmail.com>
2018-02-14 01:17:24 +0800

    k path simplest test passed

--- Makefile
+++ Makefile
@@ -89,8 +89,8 @@ TEST_GRAPH: file_reader.o rc_tree.o cppr.o $(HEADER_OBJECT) $(DATA_OBEJCT)
 	$(GCCFLAG) file_reader.o rc_tree.o cppr.o $(HEADER_OBJECT) $(DATA_OBEJCT) $(GRAPH_SRC) -o graph -DTEST_GRAPH
 	graph.exe
 
-TEST_KTH: file_reader.o $(HEADER_OBJECT) $(DATA_OBEJCT) $(GRAPH_OBJECT)
-	$(GCCFLAG) $(HEADER_OBJECT) $(DATA_OBEJCT) $(GRAPH_OBJECT) file_reader.o -o kth
+TEST_KTH: file_reader.o $(HEADER_OBJECT) $(DATA_OBEJCT) graph.o rc_tree.o cppr.o bc_map.o
+	$(GCCFLAG) $(HEADER_OBJECT) $(DATA_OBEJCT) graph.o rc_tree.o cppr.o bc_map.o file_reader.o src/graph/kth.cpp -DTEST_KTH -o kth
 	kth.exe
 
 $(HEADER_OBJECT): $(HEADER_SRC)
--- src/graph/kth.h
+++ src/graph/kth.h
@@ -11,10 +11,10 @@ class Kth {
 public:
 
     struct Edge{
-        int from, to;
+        int id, from, to;
         float delay, at_delay, delta, at_delta;
         Edge(){}
-        Edge(int f,int t,float d):from(f), to(t), delay(d), at_delay(d){}
+        Edge(int id, int f,int t,float d);
     };
 
 	struct Prefix_node {
@@ -23,32 +23,40 @@ public:
 		Edge *eptr;            // Last sidetrack in the path
 		float delta, at_delta; // Cumulative delta
 		
-		static bool compare(Prefix_node *n1, Prefix_node *n2);
+		struct Compare {
+			bool operator()(const Prefix_node *n1, const Prefix_node *n2);
+		};
 		Prefix_node(Prefix_node *p, Edge *e);
+		Prefix_node();
 	};
 	
 	struct Path {
 		// Explicit representation of a path
 		float dist;
 		vector<int> path;
+		
+		void print();
 	};
 
 	Kth(BC_map *_map, CPPR *_cppr);
     void build_from_src(const vector<pair<Transition_Type,int>>&, int src, bool only_src);
     void build_from_dest(const vector<pair<Transition_Type,int>>&, int dest, bool only_dest);
     int get_kth_id(int map_id);
+    void add_edge(int from, int to, float delay);                    // from , to in bc map
+    void add_edge(int from, int to, float delay, float clock_delay); // from , to in bc map
     
     // Kth algorithm implementation
+    void k_shortest_path(int src, int dest, int k, vector<Path> &container);
     void get_explicit_path(Path *exp_path, const Prefix_node *imp_path);
     void print();
     string get_node_name(int kth_id);
+    Kth(){} /* Just for unit test */
+	vector<vector<Edge>>& getG(); /* Just for unit test */
 
 private:
     void mark_through(const vector<pair<Transition_Type,int>>&);
     bool forward_build(int now, int next_object);
     int add_node(int bc_node_id);
-    void add_edge(int from, int to, float delay);                    // from , to in bc map
-    void add_edge(int from, int to, float delay, float clock_delay); // from , to in bc map
 
     BC_map* map;
     CPPR* cppr;
@@ -58,7 +66,7 @@ private:
     vector<bool> has_kth_id;    // if bc map id has mapped to kth id
     vector<bool> vis;           // for dfs
     vector<vector<Edge>> G;     // kth graph
-    int num_node;
+    int num_node, num_edge;
 
     vector<bool> mark;          // mark node in graph
     vector<int> object;         // the search order to build good map for kth
@@ -67,11 +75,13 @@ private:
     vector<float> dist;         // Shortest distance to dest
     vector<float> at_dist;      // For tie breaking, not real at, it means "how bad the at is", smaller value is worse
     vector<int> successor;      // Keep the shortest paht tree rooted to dest
+    vector<int> use_edge;       // Keep edge id
     int source_kth, dest_kth;   // Source and dest;
     
-    std::priority_queue<Prefix_node*, vector<Prefix_node*>, Prefix_node::compare> pq;
+    std::priority_queue<Prefix_node*, vector<Prefix_node*>, Prefix_node::Compare> pq;
+    vector<Prefix_node*> trash_can; // Store used prefix node
 
-    void build_single_dest_tree(int dest);  // Build a single destination tree rooted at "dest" (destination)
+    bool build_single_dest_tree(int dest);  // Build a single destination tree rooted at "dest" (destination)
     void get_explicit_path_helper(Path *exp_path, const Prefix_node *imp_path, int dest);
     void extend(Prefix_node *path);
     void single_dest_dfs(int v);
